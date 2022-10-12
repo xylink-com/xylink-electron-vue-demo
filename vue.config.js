7c899f34b55849001be1d9d6a34a0035dca324ec
luojinghui <luojinghui@xylink.com>
2022-10-12 20:20:55 +0800

    chore: add mac app build signature and authentication config

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,13 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   pluginOptions: {
     electronBuilder: {
       nodeIntegration: true,
-      customFileProtocol: "./",
+      customFileProtocol: './',
       builderOptions: {
-        productName: "小鱼云视频vue",
-        appId: "org.xylink.vue",
-        files: [],
+        productName: '小鱼云视频vue',
+        appId: 'com.xylink.electronapp',
         nsis: {
           oneClick: false,
           allowElevation: true,
@@ -16,68 +15,70 @@ module.exports = {
           allowToChangeInstallationDirectory: true,
           createDesktopShortcut: true,
           createStartMenuShortcut: true,
-          installerIcon: "assets/logo256.ico",
-          installerHeaderIcon: "assets/logo256.ico",
+          installerIcon: 'assets/logo256.ico',
+          installerHeaderIcon: 'assets/logo256.ico',
         },
-        asar: false,
+        asar: true,
         protocols: [
           {
-            name: "xy-vue-electron",
-            schemes: ["xy-vue-electron"],
+            name: 'xy-vue-electron',
+            schemes: ['xy-vue-electron'],
           },
         ],
         win: {
-          icon: "assets/logo256.ico",
+          icon: 'assets/logo256.ico',
           extraFiles: [
+            // 人脸失败功能需要开启
+            // {
+            //   from: './models',
+            //   to: './models',
+            //   filter: ['**/*'],
+            // },
             {
-              from: "./models",
-              to: "./models",
-              filter: ["**/*"],
+              from: 'node_modules/@xylink/xy-electron-sdk/dll',
+              to: './dll',
+              filter: ['**/*'],
             },
             {
-              from: "node_modules/@xylink/xy-electron-sdk/dll",
-              to: "./dll",
-              filter: ["**/*"],
+              from: 'node_modules/@xylink/xy-electron-sdk/dll/I420ToARGB.cso',
+              to: './',
+              filter: ['**/*'],
             },
             {
-              from: "node_modules/@xylink/xy-electron-sdk/dll/I420ToARGB.cso",
-              to: "./",
-              filter: ["**/*"],
-            },
-            {
-              from: "node_modules/@xylink/xy-electron-sdk/build/Release/",
-              to: "./resources",
-              filter: ["**/*"],
+              from: 'node_modules/@xylink/xy-electron-sdk/build/Release/',
+              to: './resources',
+              filter: ['**/*'],
             },
           ],
         },
         mac: {
-          category: "xylink.app.category.type",
-          gatekeeperAssess: false,
-          icon: "assets/logo512.png",
-          target: "dmg",
+          category: 'xylink.app.category.type',
+          icon: 'assets/logo512.png',
+          target: 'dmg',
           hardenedRuntime: true,
-          entitlements: "assets/entitlements.mac.plist",
-          entitlementsInherit: "assets/entitlements.mac.plist",
+          gatekeeperAssess: false,
+          entitlements: 'assets/entitlements.mac.plist',
+          entitlementsInherit: 'assets/entitlements.mac.plist',
           extendInfo: {
-            NSMicrophoneUsageDescription: "请允许本程序访问您的麦克风",
-            NSCameraUsageDescription: "请允许本程序访问您的摄像头",
+            NSMicrophoneUsageDescription: '请允许本程序访问您的麦克风',
+            NSCameraUsageDescription: '请允许本程序访问您的摄像头',
           },
           extraFiles: [
+            // 人脸失败功能需要开启
+            // {
+            //   from: './models',
+            //   to: './Resources',
+            //   filter: ['**/*'],
+            // },
             {
-              from: "./models",
-              to: "./Resources",
-              filter: ["**/*"],
+              from: 'node_modules/@xylink/xy-electron-sdk/dll',
+              to: './Frameworks',
+              filter: ['**/*'],
             },
             {
-              from: "node_modules/@xylink/xy-electron-sdk/dll",
-              to: "./Frameworks",
-              filter: ["**/*"],
-            },
-            {
-              from: "node_modules/@xylink/xy-electron-sdk/build/Release/",
-              to: "./resources",
-              filter: ["**/*"],
+              from: 'node_modules/@xylink/xy-electron-sdk/build/Release/',
+              to: './resources',
+              filter: ['**/*'],
             },
           ],
         },
@@ -86,21 +87,21 @@ module.exports = {
   },
   chainWebpack: (config) => {
     config.module
-      .rule("node")
+      .rule('node')
       .test(/\.node$/)
-      .use("native-ext-loader")
-      .loader("native-ext-loader")
+      .use('native-ext-loader')
+      .loader('native-ext-loader')
       .tap(() => {
         const options = {};
-        const isPrd = process.env.NODE_ENV === "production";
+        const isPrd = process.env.NODE_ENV === 'production';
 
-        options["name"] = "[name].[ext]";
+        options['name'] = '[name].[ext]';
         // 开发环境将.node的寻找路径设置到node_modules目录
 
         if (!isPrd) {
-          options["rewritePath"] = path.resolve(
+          options['rewritePath'] = path.resolve(
             __dirname,
-            "./node_modules/@xylink/xy-electron-sdk/build/Release/"
+            './node_modules/@xylink/xy-electron-sdk/build/Release/'
           );
         }
 

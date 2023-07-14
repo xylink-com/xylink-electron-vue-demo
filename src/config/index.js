e47613d70843cf30e62155ee8f1c2eb4705c25fa
lihaixing <lihaixingabc@126.com>
2023-07-14 17:28:13 +0800

    feat: v1.0.3升级 electron24 错误码整理

--- src/config/index.js
+++ src/config/index.js
@@ -12,12 +12,12 @@
  */
 
 
-export const DEFAULT_PROXY = "sdkapi.xylink.com";
+export const DEFAULT_PROXY = "testqa-sdkapi.xylink.com";
 // export const DEFAULT_PROXY = "cloud.xylink.com";
 
 export const ACCOUNT = {
-  extId: '0142901e3d83e0a1e225ef92b8663fcaebda7242',
-  clientId: 'PsdkO7Sn3ysuIZA9GiVv90D7',
-  clientSecret: '1mIkLWUf2uHkUmpoY9YJoUFXdcLGJSeH',
+  extId: '7b4cfd22962c460c2c168fc29637140f8a23cd9b',
+  clientId: '9GAhEQRjPyoXQwdEnygHhNzB',
+  clientSecret: '48f57sTtGhtT12dzNYNhp77Ib3qYSn2y',
 };
 
